8d17ca85db836a052e67eb32e49613ecea8bfd27
BaHaTech7 <raul.pardoz@gmail.com>
2024-11-09 00:36:58 -0300

    Se agrega hr a hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,6 +7,7 @@ const Hero = () => {
             <div className="leyenda">
                 <h3 className="display-2">¡Pizzería Mamma Mía!</h3>
                 <h5>¡Tenemos las mejores pizzas que podrás encontrar!</h5>
+                <hr />
             </div>
             <div className="imagen">
                 <img src={PizzaHero} alt="Pizza Hero" />
